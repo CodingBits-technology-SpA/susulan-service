abce3f19c8de3c744c055ac6c8f6cf865e330bff
PedtritoAlejos <pedroalejosb@gmail.com>
2021-08-16 17:23:59 -0400

    Se crea funcion login
    Se retorna el token con los datos del usuario en caso de ser exitoso

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,9 +16,42 @@ exports.registrarse = (req,res) => {
         return res.status(200).json( 
             formatResStandar(0,"Usuario creado con exito",[user.toJSON()]) 
         );
-
     });
   
-    
-
 };
+
+exports.login = (req, res ) => {
+
+    const {  email, password } = req.body ;
+    
+    User.findOne({email} , (error,userDB) => {
+
+        if (error || !userDB) {
+            const payloadError = formatResStandar(-10,'No existe un usuario con ese email. Por favor registrarse', []);
+            return res.status(404).json(payloadError);
+        }
+        if (!userDB.authenticate(password) ) {
+            const msgInvalidCrendentials = formatResStandar(-10,"Contraseña o email inválido", []);
+            return res.status(403).json(msgInvalidCrendentials);
+        }
+
+        const token = jwt.sign({
+            _id :userDB.id 
+        },process.env.JWT_SECRET, {
+            expiresIn:  30 * 24 * 360
+        });
+        res.cookie("t",token,{
+            expire: new Date() + 30 * 24 * 3600
+        });
+
+        const { _id , name,email } = userDB;
+        const dataUser = { token:token , user: {_id :_id,name:name , email:email} };
+        const payloadSuccess = formatResStandar(0,"Login exitoso",[dataUser]);
+
+        return res.status(200).json(payloadSuccess);
+
+        
+    });
+
+}
+

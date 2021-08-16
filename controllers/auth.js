5cf75ed8b35d2e90c0ad6ab455f3fc32465e5b4b
PedtritoAlejos <pedroalejosb@gmail.com>
2021-08-16 17:45:26 -0400

    Se crea funcion isAdmin
    Se valida si el usuario tiene el perfil admin, sino tiene se retorna un mensaje de acceso denegado para el recurso

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
-const { errorHandeler} = require("../helpers/dbErrorHandler");
 const { formatResStandar } = require("../helpers/formatRes");
 
 exports.registrarse = (req,res) => {
@@ -55,3 +54,42 @@ exports.login = (req, res ) => {
 
 }
 
+exports.isAuth = (req,res,next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth.id;
+    if (!user){
+        const msgAuthDenegation = formatResStandar(-10,"Acceso denegado",[]);
+        return res.status(403).json(msgAuthDenegation);
+    }
+
+    next();
+}
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    userProperty:"auth"
+});
+
+exports.cerrarSesion = (req,res) => {
+    const msgCloseSesion = formatResStandar(0,"Cerrar sesión con éxito",[]);
+    res.clearCookie("t");
+    return res.status(200).json(msgCloseSesion);
+}
+exports.isToken = (err,req,res,next) => {
+    
+    if(err.name === 'UnauthorizedError'){
+        const msgError  = formatResStandar(-10,"Token invalido",[]);
+        
+        return res.status(403).json(msgError);
+    }
+
+    next();
+}
+exports.isAdmin = (req,res,next) => {
+    if(req.profile.role === 0){
+        const msgAccesDenegation = formatResStandar(-10,'Acceso denegado,sólo el admin tiene acceso a este recurso.',[]);
+
+        return res.status(403).json(msgAccesDenegation);
+    }
+
+    next();
+}

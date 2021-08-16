a7461956f6b7aa264bcb4477178d7bbf7d980207
PedtritoAlejos <pedroalejosb@gmail.com>
2021-08-16 15:54:50 -0400

    Se crea la conexion a la DB
    Se implementa la conexion a la base de datos

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("HOLA MUNDO");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+const db = async () => {
+    try {
+        const suceess = await mongoose.connect(process.env.DATABASE,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useFindAndModify:false
+        });
+
+        console.log("DB conectada");
+
+    } catch (error ) {
+        console.log("DB Connection Error");
+    }
+}
+
+db();
+
+app.listen(port,() => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
+

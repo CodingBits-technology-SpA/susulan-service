c09fb72aee60be50bdb1f32423681491ab51efc2
PedtritoAlejos <pedroalejosb@gmail.com>
2021-08-21 21:44:16 -0400

    Se realiza el crud profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,18 +1,21 @@
 const Perfil = require("../models/profile")
 const { formatResStandar } = require("../helpers/formatRes");
 
-exports.profilePorId = (req,res) => {
+exports.profilePorId = (req,res,next) => {
     
     Perfil.findById(req.body.id).exec( (err,perfilDB) => {
         if(err || !perfilDB){
             const msgPefilNoExists = formatResStandar(-10,'No se encontro el perfil.',[err]);
             return res.status(404).json(msgPefilNoExists);
         }
-        const msgProfileSuccess = formatResStandar(0,'Perfil encontrado.',[perfilDB]);
-        
-        return res.status(200).json(msgProfileSuccess);
+        req.profile = perfilDB;
+        next();
     });
 }
+exports.profileLeer = (req, res) => {
+    const profileResponse = formatResStandar(0,'Perfil encontrando.',[req.profile]);
+    return res.status(200).json(profileResponse);
+};
 exports.profileLeerTodo = (req,res) => {
 
     Perfil.find().exec( (err,perfilDB) =>{
@@ -28,7 +31,31 @@ exports.profileLeerTodo = (req,res) => {
 }
 exports.profileActualizar = (req,res) => {
 
+    const profile = req.profile ;
+
+    profile.name = req.body.name;
+    profile.description = req.body.description;
+
+    profile.save( (err,profileDB) =>{
+        if(err || !profileDB){
+            const msgErrorUpdate = formatResStandar(-10,'Error al actualizar el perfil',[err]);
+            return res.status(503).json(msgErrorUpdate);
+        }
+        const msgProfileSuccess = formatResStandar(0,'Actualizado correctamente',[profileDB]);
+        return res.status(200).json(msgProfileSuccess);
+    });
+
+
 }
 exports.profileEliminar = (req,res) => {
+    const profile = req.profile;
+    profile.remove( (err,profileDB)=> {
+        if(err || !profileDB){
+            const msgErrorRemove = formatResStandar(-10,'Error al eliminar el perfil.',[]);
+            return res.status(503).json(msgErrorRemove);
+        }
+        const msgProfileSuccess = formatResStandar(0,'Eliminado correctamente.',[profileDB]);
+        return res.status(200).json(msgProfileSuccess);
+    });
 
 }
